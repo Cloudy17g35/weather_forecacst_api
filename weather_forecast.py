92cc5b9e73a3cd128530cf52ac5371cc72c8ee5b
Cloudy17g35 <ksawery.lejczak@gmail.com>
2021-09-13 10:56:32 +0200

    first commit

---
+++ weather_forecast.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = 'your_key'
+base_url = 'api.openweathermap.org/data/2.5/weather?'
+city_name = input('Your city name:> ')
+
+response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}')
+json_map = response.json()
+
+
+if response.status_code != '404':
+    print(f'Your weather for {city_name}:')
+    print()
+    print(f'Temperature (NOW) in Kelvins // Temperature in Celsius: {json_map["main"]["temp"]} K // {int(json_map["main"]["temp"]- 273)} C')
+    print()
+    print(f'Weather description: {json_map["weather"][0]["description"]}')
+    print()
+else:
+    print('City not found')
